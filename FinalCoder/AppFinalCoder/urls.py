29ad6ae1edfa049bcb823ed8eefaa40bec82efe0
BeatrizLondero <beatriz04016@gmail.com>
2022-01-25 11:09:21 -0300

    funcionan altas bajas modificacion listado

--- FinalCoder/AppFinalCoder/urls.py
+++ FinalCoder/AppFinalCoder/urls.py
@@ -1,13 +1,16 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-from AppFinalCoder.views import inicio, proveedoresAdd, proveedoresDel, proveedores
+from AppFinalCoder.views import inicio, proveedoresAdd, proveedores
 from django.urls import path
-from AppFinalCoder.views import proveedorListView
+from AppFinalCoder.views import proveedorListView, proveedorCreateView , proveedorUpdateView, proveedoresModif
+from AppFinalCoder.views import proveedorDeleteView, proveedoresBorrar
 urlpatterns = [
     path('', inicio, name="Inicio"),
     path('proveedores/list', proveedorListView.as_view(), name="proveedoresList"),
-    path('proveedores/add', proveedoresAdd, name="proveedoresAdd"),
+    path('proveedores/add', proveedorCreateView.as_view(), name="proveedoresAdd"),
     path('proveedores/', proveedores , name="proveedores"),
-    path('proveedores/del/<proveedorNombre>', proveedoresDel, name="proveedoresDel"),
-
+    path('proveedores/modificar/<pk>', proveedorUpdateView.as_view(), name="proveedoresUpdate"),
+    path('proveedores/modif>',proveedoresModif.as_view() , name="proveedoresModif"),
+    path('proveedores/borrar>',proveedoresBorrar.as_view() , name="proveedoresBorrar"),
+    path('proveedores/del/<pk>', proveedorDeleteView.as_view(), name="proveedoresDel"),
 ]
--- FinalCoder/AppFinalCoder/views.py
+++ FinalCoder/AppFinalCoder/views.py
@@ -2,7 +2,10 @@ from re import template
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic.edit import DeleteView, CreateView, UpdateView
+from django.views.generic.detail import DetailView
 
 from AppFinalCoder.models import Proveedor
 from AppFinalCoder.forms import *
@@ -29,14 +32,42 @@ def proveedoresAdd(request):
         formulario = proveedorForm()
     return render(request, 'proveedor.html',{'formulario': formulario})      
 
-def proveedoresDel(request,proveedorNombre):
-      proveedor = Proveedor.objects.get(nombre=proveedorNombre)
-      proveedor.delete()
-      return redirect('proveedores') 
+# def proveedoresDel(request,proveedorNombre):
+#       proveedor = Proveedor.objects.get(nombre=proveedorNombre)
+#       proveedor.delete()
+#       return redirect('proveedores') 
   
 class proveedorListView(ListView):
     model = Proveedor
     template_name='listado.html'  
     # context_objet_name= 'proveedores'
+
+class proveedoresModif(ListView):
+    model = Proveedor
+    template_name='proveedor_edit.html'  
     
     
+    
+class proveedorDeleteView(DeleteView):
+    # model = Proveedor
+    # success_url= reverse_lazy('proveedoresList')
+    # template_name = 'proveedor_form.html'    
+    model = Proveedor
+    success_url = reverse_lazy('proveedoresList')
+    template_name= 'proveedor_form.html' 
+    
+class proveedoresBorrar(ListView):
+    model = Proveedor
+    template_name='proveedor_borrar.html' 
+    
+class proveedorCreateView(CreateView): 
+    model = Proveedor
+    success_url = reverse_lazy('proveedoresList')
+    fields=['nombre','domicilio','email' ]
+    template_name= 'proveedor_form.html'       
+
+class proveedorUpdateView(UpdateView):    
+    model = Proveedor
+    success_url = reverse_lazy('proveedoresList')
+    fields=['nombre','domicilio','email' ]
+    template_name= 'proveedor_form.html'  
