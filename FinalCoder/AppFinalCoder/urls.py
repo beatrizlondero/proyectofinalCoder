9e7b49a75a8beebcfd5cdcebd57cc27ed3e32eb3
BeatrizLondero <beatriz04016@gmail.com>
2022-01-27 12:06:55 -0300

    con clientes

--- FinalCoder/AppFinalCoder/urls.py
+++ FinalCoder/AppFinalCoder/urls.py
@@ -1,9 +1,11 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-from AppFinalCoder.views import inicio, proveedores
+from AppFinalCoder.views import inicio, proveedores, clientes
 from django.urls import path
 from AppFinalCoder.views import proveedorListView, proveedorCreateView , proveedorUpdateView, proveedoresModif
 from AppFinalCoder.views import proveedorDeleteView, proveedoresBorrar
+from AppFinalCoder.views import clienteListView, clienteCreateView, clienteUpdateView
+from AppFinalCoder.views import clienteDeleteView, clientesBorrar, clientesModif
 urlpatterns = [
     path('', inicio, name="Inicio"),
     path('proveedores/list', proveedorListView.as_view(), name="proveedoresList"),
@@ -13,4 +15,11 @@ urlpatterns = [
     path('proveedores/modif>',proveedoresModif.as_view() , name="proveedoresModif"),
     path('proveedores/borrar>',proveedoresBorrar.as_view() , name="proveedoresBorrar"),
     path('proveedores/del/<pk>', proveedorDeleteView.as_view(), name="proveedoresDel"),
+    path('clientes/list', clienteListView.as_view(), name="clientesList"),
+    path('clientes/add', clienteCreateView.as_view(), name="clientesAdd"),
+    path('clientes/', clientes , name="clientes"),
+    path('clientes/modificar/<pk>', clienteUpdateView.as_view(), name="clientesUpdate"),
+    path('clientes/modif>',clientesModif.as_view() , name="clientesModif"),
+    path('clientes/borrar>',clientesBorrar.as_view() , name="clientesBorrar"),
+    path('clientes/del/<pk>', clienteDeleteView.as_view(), name="clientesDel"),
 ]
--- FinalCoder/AppFinalCoder/views.py
+++ FinalCoder/AppFinalCoder/views.py
@@ -10,8 +10,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from AppFinalCoder.models import Proveedor
 from AppFinalCoder.forms import *
-# from .models import Curso, Profesor
-# from AppCoder.forms import CursoForm , ProfesorForm
+from AppFinalCoder.models import Cliente
+
 @login_required
 def inicio (request):
     return render(request,'inicio.html')
@@ -20,56 +20,65 @@ def inicio (request):
 def proveedores(request):
     return render(request, 'inicio.html')
 
-# def proveedoresList(request):
-#     return render(request, 'listado.html', {'proveedor' :Proveedor.objects.all()})
-
-# def proveedoresAdd(request):
-#     if request.method=='POST':
-#         formulario = proveedorForm (request.POST)
-#         if formulario.is_valid():
-#             data = formulario.cleaned_data
-#             Proveedor.objects.create(nombre=data['nombre'], domicilio=data['domicilio'],email=data['email'])
-#             return redirect('proveedoresList')
-#     else:
-#         formulario = proveedorForm()
-#     return render(request, 'proveedor.html',{'formulario': formulario})      
-
-# def proveedoresDel(request,proveedorNombre):
-#       proveedor = Proveedor.objects.get(nombre=proveedorNombre)
-#       proveedor.delete()
-#       return redirect('proveedores') 
+def clientes(request):
+    return render(request, 'inicio.html')
   
 class proveedorListView(ListView):
     model = Proveedor
     template_name='listado.html'  
-    # context_objet_name= 'proveedores'
+    
+class clienteListView(ListView):
+    model = Cliente
+    template_name='listadocliente.html'  
 
 class proveedoresModif(LoginRequiredMixin,ListView):
     model = Proveedor
-    template_name='proveedor_edit.html'  
-    
+    template_name='proveedor_edit.html' 
+     
+class clientesModif(LoginRequiredMixin,ListView):
+    model = Cliente
+    template_name='clienteedit.html'     
     
     
 class proveedorDeleteView(DeleteView):
-    # model = Proveedor
-    # success_url= reverse_lazy('proveedoresList')
-    # template_name = 'proveedor_form.html'    
     model = Proveedor
     success_url = reverse_lazy('proveedoresList')
     template_name= 'proveedor_form_borrar.html' 
+    
+class clienteDeleteView(DeleteView):
+    model = Cliente
+    success_url = reverse_lazy('clientesList')
+    template_name= 'clienteform_borrar.html' 
     
 class proveedoresBorrar(LoginRequiredMixin, ListView):
     model = Proveedor
     template_name='proveedor_borrar.html' 
     
+class clientesBorrar(LoginRequiredMixin, ListView):
+    model = Cliente
+    template_name='clienteborrar.html' 
+    
 class proveedorCreateView(LoginRequiredMixin, CreateView): 
     model = Proveedor
     success_url = reverse_lazy('proveedoresList')
     fields=['nombre','domicilio','email' ]
-    template_name= 'proveedor_form.html'       
+    template_name= 'proveedor_form.html'  
+    
+class clienteCreateView(LoginRequiredMixin, CreateView): 
+    model = Cliente
+    success_url = reverse_lazy('clientesList')
+    fields=['nombre','apellido','nacimiento'
+            ,'email']
+    template_name= 'clienteform.html'       
 
 class proveedorUpdateView(UpdateView):    
     model = Proveedor
     success_url = reverse_lazy('proveedoresList')
     fields=['nombre','domicilio','email' ]
     template_name= 'proveedor_form.html'  
+    
+class clienteUpdateView(UpdateView):    
+    model = Cliente
+    success_url = reverse_lazy('clientesList')
+    fields=['nombre','apellido','nacimiento','email' ]
+    template_name= 'cliente_form.html'  
